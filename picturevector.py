369528fd0b949b65b71bf69795cb66d0e4d934a0
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-02-28 09:58:36 +0500

    Add files via upload

--- picturevector.py
+++ picturevector.py
@@ -3,13 +3,11 @@ from PIL import Image
 
 class Picvector():
     klist = []
-    x = 0
-    y = 0
     def __init__(self, img):
         self.img = img
-        Picvector.x = self.x
-        Picvector.y = self.y
     def vectorpixels(self):
+        global x
+        global y
         self.im = Image.open(self.img) #открываем изображение с диска
         self.pixels = self.im.load() # список с пикселями
         self.x, self.y = self.im.size # ширина (x) и высота (y) изображения
@@ -19,9 +17,13 @@ class Picvector():
                 self.klist.append(self.pixels[i,j])
                 self.pixels[i, j] = r, g, b
         return self.klist
-    def hx():
-        return Picvector.x
-    def hy():
-        return Picvector.y
+    def hx(self):
+        global x
+        x = self.x
+        return self.x
+    def hy(self):
+        global y
+        y = self.y
+        return self.y
         
         

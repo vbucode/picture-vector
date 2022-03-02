97ae19c236d6749d7d4bf75a84a966646debf7b6
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-03-02 17:37:04 +0500

    Update testpicturevector.py

--- testpicturevector.py
+++ testpicturevector.py
@@ -1,20 +1,32 @@
-from picturevector import Picvector
+#module to work with pixels
 from PIL import Image
-import json
 
-dict = {}
-
-tvi = Picvector("il1.jpg")
-ovi = Picvector("il.jpg")
-t = tvi.vectorpixels()
-o = ovi.vectorpixels()
-
-tx = tvi.hx()
-ty = tvi.hy()
-ox = ovi.hx()
-oy = ovi.hy()
-print(t)
-dict["bm"] = t
-with open("bitmaps.json", "a") as file:
-    json.dump(dict, file)
-    print("save!")
+class Picvector():
+    klist = []
+    xlist = []
+    ylist = []
+    def __init__(self, img):
+        self.img = img
+    def vectorpixels(self):
+        global x
+        global y
+        self.im = Image.open(self.img) # open file from disk
+        self.pixels = self.im.load() # list of pixels
+        self.x, self.y = self.im.size # x y sizes picture
+        for i in range(self.y):
+            self.klist.append([0]*self.x)
+        for i in range(self.x):
+            for j in range(self.y):
+                r, g, b = self.pixels[i, j]
+                self.pixels[i, j] = 255 - r, 255 - g, 255 - b
+                self.klist[j][i] = self.pixels[i, j]
+        #self.im.save(self.img + "-1" + ".jpg") # this option is optional
+        return self.klist
+    def hx(self):
+        global x
+        x = self.x
+        return self.x
+    def hy(self):
+        global y
+        y = self.y
+        return self.y

e62a0a1bfcee0daab95e4b23609fab6346fd37b9
Bulat Valiakhmetov <61677060+Bulatv2@users.noreply.github.com>
2022-03-02 17:21:49 +0500

    Update picturevector.py

--- picturevector.py
+++ picturevector.py
@@ -3,7 +3,6 @@ from PIL import Image
 
 class Picvector():
     klist = []
-    slist = []
     xlist = []
     ylist = []
     def __init__(self, img):
@@ -14,22 +13,15 @@ class Picvector():
         self.im = Image.open(self.img) # open file from disk
         self.pixels = self.im.load() # list of pixels
         self.x, self.y = self.im.size # x y sizes picture
+        for i in range(self.y):
+            self.klist.append([0]*self.x)
         for i in range(self.x):
             for j in range(self.y):
                 r, g, b = self.pixels[i, j]
                 self.pixels[i, j] = 255 - r, 255 - g, 255 - b
-                self.slist.append(self.pixels[i, j])
-            self.klist.append(self.slist)
-            del self.slist[self.x:]
+                self.klist[j][i] = self.pixels[i, j]
         #self.im.save(self.img + "-1" + ".jpg") # this option is optional
-        for i in self.klist:
-            for j in i:
-                if sum(j) <= 6:
-                    self.ylist.append(0)
-                self.ylist.append(1)
-            self.xlist.append(self.ylist)
-            del self.ylist[len(i):]
-        return self.xlist
+        return self.klist
     def hx(self):
         global x
         x = self.x
@@ -38,5 +30,3 @@ class Picvector():
         global y
         y = self.y
         return self.y
-        
-        

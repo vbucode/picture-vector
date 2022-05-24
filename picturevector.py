0715a9c6964701ca5be60be71f7d832b1c20901b
vbucode <61677060+vbucode@users.noreply.github.com>
2022-05-24 23:19:09 +0500

    Add files via upload

--- picturevector.py
+++ picturevector.py
@@ -12,16 +12,15 @@ class Picvector():
         self.x, self.y = self.im.size # x y sizes picture
         for i in range(self.x):
             tlist = []
-            tlist2 = []
             for j in range(self.y):
+                tlist2 = []
                 r, g, b = self.pixels[i, j]
                 self.pixels[i, j] = r, g, b
                 if sum(self.pixels[i, j]) <= 700:
-                    tlist.append([1])
+                    tlist.append(1)
                 else:
-                    tlist.append([0])
-            for k in tlist:
-                tlist2.extend(k)
+                    tlist.append(0)
+            tlist2.extend(tlist)
             klist.append(tlist2)
 
         #self.im.save(self.img + "-1" + ".jpg") # this option is optional
